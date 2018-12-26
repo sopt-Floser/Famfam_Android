580117d90c9c9caf4a2e77434765385a42c4d2ef
2Re-play <wogusduddnjs@gmail.com>
2018-12-26 22:34:26 +0900

    complete certification VIEW

--- app/src/main/java/com/sopt/famfam/activity/CertificationActivity.kt
+++ app/src/main/java/com/sopt/famfam/activity/CertificationActivity.kt
@@ -2,12 +2,29 @@ package com.sopt.famfam.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.sopt.famfam.R
+import android.widget.AdapterView
+import android.widget.Spinner
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_certification.*
+
 
 class CertificationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_certification)
+        sp_certification_act_select_telecom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                        var telecom = parent!!.getItemAtPosition(position) // 통신사 뽑아내기
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                //
+            }
+        }
+
     }
 }
