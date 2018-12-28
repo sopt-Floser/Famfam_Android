f2952064f4f27071a4e088b3ede09179723bca0c
2Re-play <wogusduddnjs@gmail.com>
2018-12-28 19:37:49 +0900

    complete intro VIEW

--- app/src/main/java/com/sopt/famfam/fragment/IntroFragmentStatePagerAdapter.kt
+++ app/src/main/java/com/sopt/famfam/fragment/IntroFragmentStatePagerAdapter.kt
@@ -4,12 +4,15 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 
+
 class IntroFragmentStatePagerAdapter(fm: FragmentManager, val fragmentCount: Int) : FragmentStatePagerAdapter(fm) {
+
     override fun getItem(position: Int): Fragment? {
         when (position) {
             0 -> return IntroFirstFragment()
             1 -> return IntroSecondFragment()
             2 -> return IntroThirdFragment()
+            3 -> return IntroFourthFragment()
             else -> return null
         }
     }
