5746a22f01847945b8654637b0e5629d13928404
2Re-play <wogusduddnjs@gmail.com>
2019-01-09 17:27:13 +0900

    modified network deleteUser

---
+++ app/src/main/java/com/sopt/famfam/activity/AccountSecurityActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.famfam.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AccountSecurityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_security)
+    }
+}
